e0cda80b411a5185062fe79da14040a066cd917d
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-10-24 12:46:22 -0500

    🟩 Si_HayUnaCelulaViva_Y_TieneMasDeTresVecinos_Debe_Morir

--- GameOfLife.cs
+++ GameOfLife.cs
@@ -30,7 +30,7 @@ public class GameOfLife(bool[,] universe)
                         numberOfAliveNeighbors++;
                     if (_universe[row + 1, column - 1])
                         numberOfAliveNeighbors++;
-                    if (numberOfAliveNeighbors < 2)
+                    if (numberOfAliveNeighbors < 2 || numberOfAliveNeighbors > 3)
                         _universe[row, column] = false;
                     else
                         _universe[row, column] = true;
