09a8e080d0df117412d835feceb2e74359e4d846
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-10-24 12:01:45 -0500

    🟦 Si_HayUnaCelulaViva_Y_TieneDosOTresVecinos_Debe_Vivir

--- GameOfLife.cs
+++ GameOfLife.cs
@@ -7,33 +7,31 @@ public class GameOfLife(bool[,] universe)
     public void nextGen()
     {
         
-        for (int row = 0; row < _universe.GetLength(0); row++)
+        for (var row = 0; row < _universe.GetLength(0); row++)
         {
-            for (int column = 0; column < _universe.GetLength(1); column++)
+            for (var column = 0; column < _universe.GetLength(1); column++)
             {
-                bool cell = _universe[row, column];
-                if (cell)
-                {
-                    var numberOfAliveNeighbors = 0;
-                    if (_universe[row - 1, column + 1])
-                        numberOfAliveNeighbors++;
-                    if (_universe[row, column + 1])
-                        numberOfAliveNeighbors++;
-                    if (_universe[row +1 , column + 1])
-                        numberOfAliveNeighbors++;
-                    if (_universe[row - 1, column])
-                        numberOfAliveNeighbors++;
-                    if (_universe[row + 1, column])
-                        numberOfAliveNeighbors++;
-                    if (_universe[row - 1, column - 1])
-                        numberOfAliveNeighbors++;
-                    if (_universe[row, column - 1])
-                        numberOfAliveNeighbors++;
-                    if (_universe[row + 1, column - 1])
-                        numberOfAliveNeighbors++;
-                    if (numberOfAliveNeighbors != 2)
-                        _universe[row, column] = false;
-                }
+                var cell = _universe[row, column];
+                if (!cell) continue;
+                var numberOfAliveNeighbors = 0;
+                if (_universe[row - 1, column + 1])
+                    numberOfAliveNeighbors++;
+                if (_universe[row, column + 1])
+                    numberOfAliveNeighbors++;
+                if (_universe[row +1 , column + 1])
+                    numberOfAliveNeighbors++;
+                if (_universe[row - 1, column])
+                    numberOfAliveNeighbors++;
+                if (_universe[row + 1, column])
+                    numberOfAliveNeighbors++;
+                if (_universe[row - 1, column - 1])
+                    numberOfAliveNeighbors++;
+                if (_universe[row, column - 1])
+                    numberOfAliveNeighbors++;
+                if (_universe[row + 1, column - 1])
+                    numberOfAliveNeighbors++;
+                if (numberOfAliveNeighbors != 2)
+                    _universe[row, column] = false;
             }
         }
     }
--- TestList.md
+++ TestList.md
@@ -1,5 +1,5 @@
 ﻿- [x] Un universo vacio debe retornar un universo vacio
 - [x] Una celula viva con menos de dos vecinas vivas, muere
-- [ ] Una celula viva con dos o tres vecinas vivas, vive
+- [x] Una celula viva con dos o tres vecinas vivas, vive
 - [ ] Una celula viva con mas de tres vecinas vivas, muere
 - [ ] Una celula muerta con exactamente tres vecinas vivas, vive
